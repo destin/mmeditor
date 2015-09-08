d8f8614fefe4b1a850cdeab35ec469f481677d72
destin <pytel.dawid@gmail.com>
2015-09-08 15:19:10 +0200

    Organized imports

--- app/src/main/java/com/dpytel/android/mmeditor/MindmapExpandableAdapter.java
+++ app/src/main/java/com/dpytel/android/mmeditor/MindmapExpandableAdapter.java
@@ -2,7 +2,6 @@ package com.dpytel.android.mmeditor;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
